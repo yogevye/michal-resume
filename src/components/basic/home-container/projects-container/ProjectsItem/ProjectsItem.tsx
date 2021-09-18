8d78aa486fa9c755bdeb02cd5daf5c99566c80d9
yogevye <yogevye1@gmail.com>
2021-09-18 18:31:02 +0300

    finish web home

--- src/components/basic/home-container/projects-container/ProjectsItem/ProjectsItem.tsx
+++ src/components/basic/home-container/projects-container/ProjectsItem/ProjectsItem.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './ProjectsItem.css';
 import {Link} from "react-router-dom";
-
 const viewCaseStudyMessage = 'View Case Study ->';
-const imgTest = import('../../../../../images/collection_mobile.jpeg');
 
 
 // @ts-ignore
 const ProjectsItem = ({projectLink}) => (
     <Link to={projectLink.link} className="ProjectLink">
         <div className="ProjectsItem">
-            <div className="ProjectLogoContainer">
-                <img src={projectLink.logo} alt="logo"  className="ProjectLogo"/>
-            </div>
+            <div className="ProjectLogoContainer" style={{backgroundImage: 'url(' + projectLink.logo + ')'}}/>
             <div className="CardContainer">
                 <div className="ProjectDescription">
                     <div className="new-line redesignText">
